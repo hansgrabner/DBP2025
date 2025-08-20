b818f44949d929c6cc5a36e97f875527fbf75226
Johann Grabner <johann.grabner@lv.campus02.at>
2025-08-20 07:59:19 +0200

    DBP2025 - DriverManager - Connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        connect();
         System.out.println("Hello, World!");
+    }
+
+    public static void connect() {
+        // connection string
+        var url = "jdbc:sqlite:C:/LVs/DBP2025/chinook.db";
+
+        try (var conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
